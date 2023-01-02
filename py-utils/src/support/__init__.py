ece93fe74d8994fda00a9c52ce5c4066811a2f6c
root <root@iu34-r6.pun.seagate.com>
2023-01-02 08:34:18 +0530

    S3bench script added

--- py-utils/src/support/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Support package containing utils Support Bundle executables.
-
-contains:
-cortx_support_bundle.py - triggers node wise support bundle.
-support.yaml            - support.yaml config file for utils.
-utils_support_bundle.py - triggers support bundle for utils component.
-"""
